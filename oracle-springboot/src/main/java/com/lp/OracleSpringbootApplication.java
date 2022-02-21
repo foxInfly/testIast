9788463c96f2c433d1aee372988ee05eb3d4fae5
lp-mac <122792286@qq.com>
2022-02-21 14:18:34 +0800

    Initial commit

---
+++ oracle-springboot/src/main/java/com/lp/OracleSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.lp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OracleSpringbootApplication.class, args);
+    }
+
+}
